a8f55ee660b07f6602b39d4cff1935f97e8bcaf4
Astrid de Boissieu <astrid.de-boissieu@hec.edu>
2018-03-20 15:27:16 +0100

    trips_actvities

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :trips, only: [:create, :index] do
-    resources :trip_activities, only: [:new, :create]
+    resources :trip_activities, only: [:index, :new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
