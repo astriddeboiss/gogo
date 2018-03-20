57a1f2a5fe281d2fee72d217f772b6625f4797a7
Marie-Lilas Onanga-Ozavino <marie-lilas.onanga-ozavino@hec.edu>
2018-03-20 19:09:59 +0100

    mark as done

--- app/controllers/trip_activities_controller.rb
+++ app/controllers/trip_activities_controller.rb
@@ -4,4 +4,22 @@ class TripActivitiesController < ApplicationController
 
   def ceate
   end
+
+  def visited
+    @trip_activities.trip = current_user.trips.find(params[:trip_id])
+    @trip_activity.activity.mark_as_done = true
+    @trip_activity.save
+    # redirect_to current_user
+  end
+
+  def not_visited
+    @trip_activity.trip = current_user.trips.find(params[:trip_id])
+    @trip_activity.activity.mark_as_done = false
+    @trip_activity.save
+
+    # @trip_activity = current_user.trip_activities.find(params[:trip_id])
+    # @trip_activity.activity.mark_as_done = false
+    # @trip_activity.save
+    # redirect_to current_user
+  end
 end
