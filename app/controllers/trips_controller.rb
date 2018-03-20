f266d90cec2c6f33e8d506a5979342280f54edb0
antoine picard <antn.picard@gmail.com>
2018-03-20 13:05:31 +0100

    i created a form to chose your city and create your trip

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,21 @@
+class TripsController < ApplicationController
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user = current_user
+    @test = @trip.valid?
+    raise
+    if @trip.save
+      raise
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def trip_params
+   params.require(:trip).permit(:city)
+  end
+
+
+end
