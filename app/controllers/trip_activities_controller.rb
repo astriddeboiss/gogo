652bb1ffc63c78d37747d8338cda5c516c630633
Boualam Allouche <alloucheboualam@gmail.com>
2018-03-21 15:32:38 +0100

    created map with activiies markers on the trip show

--- app/controllers/trip_activities_controller.rb
+++ app/controllers/trip_activities_controller.rb
@@ -1,7 +1,15 @@
 class TripActivitiesController < ApplicationController
   def index
     @trip = Trip.find(params[:trip_id])
-    @trip_activities = TripActivity.all
+    @trip_activities = TripActivity.where.not(latitude: nil, longitude: nil)
+
+    @markers = @trip_activities.map do |trip_activity|
+      {
+        lat: trip_activity.latitude,
+        lng: trip_activity.longitude
+        # infoWindow: { content: render_to_string(partial: "/trip_activities/map_box", locals: { trip_activity: trip_activity }) }
+      }
+    end
   end
 
   def new
