7337fb9d805d2296368266d5618f29ca3aa8e76f
Boualam Allouche <alloucheboualam@gmail.com>
2018-03-28 16:45:20 +0200

    checklist demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+  end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
    end
