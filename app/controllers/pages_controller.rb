2bca8f71ad484ab780d7ded864a6a495368aec9c
antoine picard <antn.picard@gmail.com>
2018-03-28 15:14:18 +0200

    moved the gogo actikons to trip activity controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,30 +5,9 @@ class PagesController < ApplicationController
   def home
   end
 
-  def gogo
-  end
-
-  def choice_trip
-    # require 'rest_client'
-    # url = "https://www.googleapis.com/geolocation/v1/geolocate?key=#{ENV["GOOGLE_API_SERVER_KEY"]}"
-    # response = RestClient.post url, {}
-    # serialized_response = JSON.parse(response)
-    # lat = serialized_response.first.last["lat"]
-    # lng = serialized_response.first.last["lng"]
-    lat = params[:latitude]
-    lng = params[:longitude]
-    address_components = Geocoder.search("#{lat}, #{lng}").first.data["address_components"]
-    city_hash = address_components.detect {|element| element.has_key?("types") && element["types"] == ["locality", "political"]}
-    city = City.find_by_name(city_hash["long_name"])
-    @choices = Trip.where(city: city, user: current_user)
-  end
-
   def profile
   end
 
-  def city_find
-
-  end
 
 end
 
--- app/controllers/trip_activities_controller.rb
+++ app/controllers/trip_activities_controller.rb
@@ -45,6 +45,27 @@ class TripActivitiesController < ApplicationController
     redirect_to trip_trip_activities_path(@trip_activity.trip)
   end
 
+  def gogo
+  end
+
+  def choice_trip
+    # require 'rest_client'
+    # url = "https://www.googleapis.com/geolocation/v1/geolocate?key=#{ENV["GOOGLE_API_SERVER_KEY"]}"
+    # response = RestClient.post url, {}
+    # serialized_response = JSON.parse(response)
+    # lat = serialized_response.first.last["lat"]
+    # lng = serialized_response.first.last["lng"]
+    lat = params[:latitude]
+    lng = params[:longitude]
+    address_components = Geocoder.search("#{lat}, #{lng}").first.data["address_components"]
+    city_hash = address_components.detect {|element| element.has_key?("types") && element["types"] == ["locality", "political"]}
+    city = City.find_by_name(city_hash["long_name"])
+    @choices = Trip.where(city: city, user: current_user)
+  end
+
+  def city_find
+  end
+
   private
 
   def params_trip_activity
